dd9f1123baf1ab8f7d2f945ddd7aed255c3855d2
i-am-amal <amald1011@gmail.com>
2025-11-25 15:55:58 +0530

    Media file picking functionality integrated

--- lib/views/create_ad_screen/create_ad_screen.dart
+++ lib/views/create_ad_screen/create_ad_screen.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class CreateAdScreen extends StatefulWidget {
@@ -12,6 +13,73 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController redirectUrlController = TextEditingController();
   final TextEditingController locationController = TextEditingController();
+
+  // List<PlatformFile> selectedFiles = [];
+
+  // Future<void> pickMedia() async {
+  //   final result = await FilePicker.platform.pickFiles(
+  //     allowMultiple: true,
+  //     type: FileType.custom,
+  //     allowedExtensions: ['jpg', 'jpeg', 'png', 'mp4', 'mov'],
+  //   );
+
+  //   if (result != null) {
+  //     setState(() {
+  //       selectedFiles = result.files;
+  //     });
+  //   }
+  // }
+  static const imageMaxSize = 5 * 1024 * 1024; // 5MB
+  static const videoMaxSize = 20 * 1024 * 1024; // 20MB
+
+  void _showError(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
+  Future<void> pickMedia() async {
+    final result = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+      type: FileType.custom,
+      allowedExtensions: ['jpg', 'jpeg', 'png', 'mp4', 'mov'],
+      withData: true, // IMPORTANT for Flutter Web to access file.bytes
+    );
+
+    if (result != null) {
+      List<PlatformFile> validFiles = [];
+
+      for (var file in result.files) {
+        final ext = file.extension?.toLowerCase();
+
+        // Determine if it's image or video
+        final isImage = ['jpg', 'jpeg', 'png'].contains(ext);
+        final isVideo = ['mp4', 'mov'].contains(ext);
+
+        // Check size
+        if (isImage && file.size > imageMaxSize) {
+          _showError("${file.name} is larger than 5MB");
+          continue;
+        }
+        if (isVideo && file.size > videoMaxSize) {
+          _showError("${file.name} is larger than 20MB");
+          continue;
+        }
+
+        validFiles.add(file);
+      }
+
+      setState(() {
+        selectedFiles = validFiles;
+      });
+    }
+  }
+
+  List<PlatformFile> selectedFiles = [];
 
   @override
   Widget build(BuildContext context) {
@@ -30,54 +98,92 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
             const SizedBox(height: 10),
 
             Center(
-              child: Container(
-                width: 600,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  border: Border.all(color: Colors.grey.shade300, width: 1.4),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.grey.shade50, Colors.grey.shade200],
+              child: InkWell(
+                onTap: pickMedia,
+                borderRadius: BorderRadius.circular(18),
+                child: Container(
+                  width: 600,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    border: Border.all(color: Colors.grey.shade300, width: 1.4),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Colors.grey.shade50, Colors.grey.shade200],
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.05),
+                        blurRadius: 8,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 0.05),
-                      blurRadius: 8,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.cloud_upload_outlined,
-                      size: 58,
-                      color: Colors.blueGrey.shade600,
-                    ),
-                    const SizedBox(height: 12),
-                    Text(
-                      "Tap to Upload Media",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.grey.shade900,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.cloud_upload_outlined,
+                        size: 58,
+                        color: Colors.blueGrey.shade600,
                       ),
-                    ),
-                    const SizedBox(height: 6),
-                    Text(
-                      "Images or Videos (Drag & Drop Supported)",
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey.shade600,
+                      const SizedBox(height: 12),
+                      Text(
+                        "Tap to Upload Media",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.grey.shade900,
+                        ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 6),
+                      Text(
+                        "Images or Videos",
+                        // (Drag & Drop Supported)",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey.shade600,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
+            const SizedBox(height: 30),
+
+            if (selectedFiles.isNotEmpty)
+              Wrap(
+                spacing: 12,
+                runSpacing: 12,
+                children:
+                    selectedFiles.map((file) {
+                      final isImage =
+                          file.extension == 'jpg' ||
+                          file.extension == 'png' ||
+                          file.extension == 'jpeg';
+
+                      return Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.grey.shade300),
+                        ),
+                        child:
+                            isImage
+                                ? Image.memory(file.bytes!, fit: BoxFit.cover)
+                                : const Center(
+                                  child: Icon(
+                                    Icons.videocam,
+                                    size: 40,
+                                    color: Colors.blueGrey,
+                                  ),
+                                ),
+                      );
+                    }).toList(),
+              ),
             const SizedBox(height: 30),
 
             // ------------------ TITLE ------------------
